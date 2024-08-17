08c715de187eafe5c2da143133764a5b6bb1c03a
mateusz29 <strzeleckimateusz10@gmail.com>
2024-08-17 16:11:44 +0200

    Designed CNN architecture

---
+++ CNN.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.functional as F
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(128 * 6 * 6, 512)
+        self.fc2 = nn.Linear(512, 7)  # 7 output classes
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 128 * 6 * 6)  # Flatten the tensor
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
--- train_model.py
+++ train_model.py
@@ -1,8 +1,10 @@
 from data_loader import load_data
+import torch
 
 def main() -> None:
     # Load the data
     train_loader, test_loader = load_data(batch_size=64)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 if __name__ == '__main__':
     main()
